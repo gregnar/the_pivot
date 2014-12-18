840bec0c8ed6fa05b1ef361e3af6770a98a55f14
Chandra Carney <chandracarney@me.com>
2014-12-18 16:32:02 -0700

    changes to supplier spec

--- spec/integration/happy_path/supplier_spec.rb
+++ spec/integration/happy_path/supplier_spec.rb
@@ -3,35 +3,34 @@ require 'capybara/rails'
 require 'capybara/rspec'
 
 
-  feature "menu_items things" do
-    let :user do
-      User.create name: 'Supplying Things', email: 'supplier@example.com', password: 'p4ssw0rd', password_confirmation: 'p4ssw0rd', supplier: true
-    end
-
-    before(:each) do
-      visit '/login'
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
-      click_button 'Login'
-    end
+feature "menu_items things" do
+  let :user do
+    User.create name: 'Hamburgler', email: 'hamburgler@example.com', password: 'p4ssw0rd', password_confirmation: 'p4ssw0rd', supplier_id: true
   end
 
-describe 'Unauthenticated visitor happy path', type: :feature do
+  before(:each) do
+    visit '/login'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Login'
+  end
+end
+
+describe 'supplier happy path', type: :feature do
 
   context "when visitor visits a business" do
     let(:torta_shop) do
       Supplier.create(name: "Torta's Supplies",
       email:        "tortaville@mexicana.net",
       phone:        "1-(720)-303-6098",
-      fax:          "1-(720)-946-7839",
       description:  "We are a super-special service for airdropped mexican sandwiches.
       They raise morale and promote Jorge's happiness!"
       )
     end
   end
 
-  scenario "User creates a new menu_item" do
-    visit "/menu_items"
+  scenario "supplier creates a new item" do
+    visit "/torta-s-supplies/items"
     new_menu_item = {title: "I created a menu_item", description: "full of yummy", price: 10000}
     expect(page.body).not_to include new_menu_item[:title]
     click_link_or_button "New"
@@ -42,16 +41,12 @@ describe 'Unauthenticated visitor happy path', type: :feature do
     expect(page.body).to include new_menu_item[:title]
   end
 
-  it "allows suppliers to view their page" do
-
+  scenario "allows suppliers to view their page" do
     visit "/torta-s-supplies"
     expect(page).to have_content("Torta's Supplies")
-
   end
 
-  it 'can add a new item as a supplier' do
-    let
-    # category.save
+  scenario 'can add a new item as a supplier' do
     visit new_item_path
     fill_in 'Title', with: 'Sandwich Bags'
     fill_in 'Description', with: '20 lb Black Sandwich Bag'
