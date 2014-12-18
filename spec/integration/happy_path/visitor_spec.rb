eaafc677b6adcea420ed20a5ca0788d4bbb91fba
Herbert Joseph <techniquea2z@gmail.com>
2014-12-18 10:44:55 -0700

    progress on visitor spec, need a rebase

--- spec/integration/happy_path/visitor_spec.rb
+++ spec/integration/happy_path/visitor_spec.rb
@@ -11,15 +11,50 @@ describe 'Unauthenticated visitor happy path', type: :feature do
               phone:        "1-(720)-303-6098",
               fax:          "1-(720)-946-7839",
               description:  "We are a super-special service for airdropped mexican sandwiches.
-                            They raise morale and promote Jorge's happiness!"
-      )
+                            They raise morale and promote Jorge's happiness!")
     end
 
-    visit suppliers_path
-    expect(page).to have_content("Torta's Supplies")
+    it "has the list of all suppliers" do
+      visit '/suppliers'
+      expect(page).to have_content("Torta's Supplies")
+    end
 
-    click_link_or_button "Torta's Supplies"
-    expect(current_path).to eq(torta_path.slug)
-    expect(page).to have_content("Torta's Supplies")
+    it "goes to the correct page" do
+      click_link_or_button "Torta's Supplies"
+      expect(current_path).to eq(torta_path.slug)
+      expect(page).to have_content("Torta's Supplies")
+    end
+  end
+
+  context "when visitor shops" do
+    before(:all) do
+      let(:torta_shop) do
+        Supplier.create(name: "Torta's Supplies",
+        email:        "tortaville@mexicana.net",
+        phone:        "1-(720)-303-6098",
+        fax:          "1-(720)-946-7839",
+        description:  "We are a super-special service for airdropped mexican sandwiches. 
+        They raise morale and promote Jorge's happiness!")
+      end
+      let(:nba_cares) do
+        Supplier.create(name: "NBA Cares",
+        email:        "donaldsterling@laclippers.com",
+        phone:        "1-(303)-693-9048",
+        fax:          "1-(720)-590-3204",
+        description:  "We joined Airlyft because we believe in basketball's healing quality 
+        Do you want to enrich children all over? Use NBA supplies to build high-quality courts wholewide!")
+      end
+      Item.create!(title: "NBA Hoop",
+      description: 'Now with Shaq-proof backboards',
+      price: Faker::Commerce.price,
+      photo_file_name: nil,
+      photo_content_type: 'image/png',
+      photo_file_size: Faker::Number.number(3),
+      photo_updated_at: Faker::Date.between(1.week.ago, Date.today),
+      categories: [Category.all.sample],
+      supplier_id: nba_cares.id,
+      active: true
+      )
+
   end
 end
