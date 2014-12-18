3bfa93aaf9e8173138488353de099dfb129c6840
Greg Narajka <gregnar@gmail.com>
2014-12-18 13:26:21 -0700

    added migrations to fix users supplier column

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
- # encoding: UTF-8
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217001910) do
+ActiveRecord::Schema.define(version: 20141218202215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,7 +106,7 @@ ActiveRecord::Schema.define(version: 20141217001910) do
     t.boolean  "admin",           default: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "supplier"
+    t.boolean  "supplier_admin",  default: false
   end
 
 end
