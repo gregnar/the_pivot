faa9529978f2dab2bb6f3c54d190ea8dd2c31c49
Herbert Joseph <techniquea2z@gmail.com>
2014-12-31 10:07:37 -0700

    just need me a rebasE

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,9 @@ Rails.application.routes.draw do
 
   resources :items, path: "/supplies"
 
+  match 'register' => 'users#new'
   resources :users
+  get '/account_confirmation', to: 'users#account_confirmation'
 
   resources :password_resets
 
