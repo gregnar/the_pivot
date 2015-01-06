7c6dbef2f1aa3e44bcece86bc8092d20fbe1c18b
Chandra Carney <chandracarney@me.com>
2015-01-06 16:21:46 -0700

    cart item quantity update working on click, what whattttt

--- app/assets/javascripts/carts.js
+++ app/assets/javascripts/carts.js
@@ -1,13 +1,38 @@
-function addItem(item) {
-    console.log('+1');
-    $.post('cart_items/plus', {
-      item: item
-    })
-  }
+$(function () {
+  $('.add-item').on('click', addItem);
+  $('.remove-item').on('click', removeItem);
+});
 
-  function removeItem(item) {
-    console.log('-1');
-    $.post('cart_items/minus', {
-      item: item
-    })
-  }
+function addItem(event) {
+  console.log('+1');
+
+  event.preventDefault;
+
+  var $item = $(this);
+  var itemId = $item.data('item_id');
+
+  $.post('cart_items/plus', {
+    item: itemId
+  }).then(function () {
+    var $itemCount = $item.siblings('.item-count')
+    var count = parseInt($itemCount.text(), 10);
+    $itemCount.text(++count);
+  });
+}
+
+function removeItem(item) {
+  console.log('-1');
+
+  event.preventDefault;
+
+  var $item = $(this);
+  var itemId = $item.data('item_id');
+
+  $.post('cart_items/minus', {
+    item: itemId
+  }).then(function () {
+    var $itemCount = $item.siblings('.item-count')
+    var count = parseInt($itemCount.text(), 10);
+    $itemCount.text(--count);
+  });
+}
