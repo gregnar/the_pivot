ea8e0f7ad3517caa3f528e9d9f3a9815ab95b407
Greg Narajka <gregnar@gmail.com>
2015-01-06 10:41:07 -0700

    changed validation on item_orders to callback

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,5 +1,10 @@
 class ItemOrder < ActiveRecord::Base
   belongs_to :order
   belongs_to :item
+  after_save :delete_if_no_item_id
+
+  def delete_if_no_item_id
+    destroy if item_id.nil? || !item.present?
+  end
 
 end
