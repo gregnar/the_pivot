ba3e0b4fdea4672b20ecedaca9b6bbda3f74abec
Herbert Joseph <techniquea2z@gmail.com>
2015-01-06 11:16:07 -0700

    finished new header

--- app/assets/javascripts/burger_menu.js
+++ app/assets/javascripts/burger_menu.js
@@ -1,22 +1,9 @@
-/*
-Slidemenu
-*/
-(function() {
-  var $body = document.body
-  , $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
-
-  if ( typeof $menu_trigger !== 'undefined' ) {
-    $menu_trigger.addEventListener('click', function() {
-      $body.className = ( $body.className == 'menu-active' )? '' : 'menu-active';
-    });
-  }
-
-}).call(this);
-
 $( document ).ready(function() {
-  var hamburger = $('#hamburger-icon');
-  hamburger.click(function() {
-    hamburger.toggleClass('active');
-    return false;
+  var $menuTrigger = $('.menu-trigger');
+  var $body = $('body');
+
+  $menuTrigger.unbind('click').click(function(){
+    $body.toggleClass('menu-active');
   });
+
 });
