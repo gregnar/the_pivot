f5173ee2288cad1fc5befe170b900e9c2c526abd
Chandra Carney <chandracarney@me.com>
2015-01-06 09:24:52 -0700

    maps working without cluttering views

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -1,5 +1,7 @@
+
+
 $(window).load(function() {
-  loadScript();
+  initialize();
 });
 
 var map;
@@ -29,13 +31,3 @@ function initialize() {
 
 
 }
-
-function initialize() {
-  var mapOptions = {
-    center: { lat: -34.397, lng: 150.644},
-    zoom: 8
-  };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-  mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
