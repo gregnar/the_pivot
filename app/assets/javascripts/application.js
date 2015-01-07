345e5fb6bf56f941a827a770bcf995d603a3c58a
Greg Narajka <gregnar@gmail.com>
2015-01-07 13:19:40 -0700

    forget turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
 //= require cocoon
 //= require bootstrap.min
 //= require_tree .
